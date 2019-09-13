7a369b41498bb14fccff5487bd4233f3600655e2
Ken Sodemann <ken@ionic.io>
2019-09-13 10:48:03 -0500

    add a refresh

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  items = [
+    {
+      name: 'Oscar',
+      description: 'he is a little grouchy'
+    },
+    {
+      name: 'Grover',
+      description: 'he is a little cute'
+    },
+    {
+      name: 'Big Bird',
+      description: 'he is a little annoying'
+    }
+  ];
 
-  constructor() {}
-
+  doRefresh(evt: Event) {
+    setTimeout(() => {
+      this.items.push({
+        name: 'Oscar',
+        description: 'he is a little grouchy'
+      });
+      this.items.push({
+        name: 'Grover',
+        description: 'he is a little cute'
+      });
+      this.items.push({
+        name: 'Big Bird',
+        description: 'he is a little annoying'
+      });
+      (evt.target as any).complete();
+    }, 750);
+  }
 }
